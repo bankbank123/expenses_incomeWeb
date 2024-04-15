a8202a84430e40c3587fb58bcaeb726b772d08c3
bankbank123 <chidchanun.bank@mail.kmutt.ac.th>
2024-04-16 06:25:57 +0700

    update 16/4/2567

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from './pages/Login.jsx';
+import Home from './pages/Home.jsx';
+import Signup from './pages/Signup.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/signup",
+    element: <Signup/>
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
