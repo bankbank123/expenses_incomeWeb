5e658d3e4e9fc9c1a112271d2047f044788cd1b5
bankbank123 <chidchanun.bank@mail.kmutt.ac.th>
2024-04-16 06:01:47 +0700

    update 16/4/2567

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
 
   return (
     <>
-      
+      <Router>
+
+
+
+        <Routes> 
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+
+      </Router>
     </>
   )
 }
